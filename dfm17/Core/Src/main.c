71d2ef8cd773e67cd771e1fce1fa27fec70f2142
Patrick van Staveren <trick@vanstaveren.us>
2023-03-28 23:29:48 -0500

    at this point we're writing to the fifo, it's pipping out what looks like two letters of data, and then nothing else, even if i bang more stuff into the fifo. I think my writeFIFO function doesn't work for any length of buffer.

--- dfm17/Core/Src/main.c
+++ dfm17/Core/Src/main.c
@@ -299,8 +299,8 @@ void process_rtty_tick()
 
 void STABBY_ook(void) {
 // ported from si_fifo_feeder_thd radio.c:247
-	//char msg[] = "hello from KD9PRC hello from KD9PRC hello from KD9PRC ";
-    uint8_t msg[] = { 0x00, 0x00, 0x00, 0x01, 0x01 };
+	char msg[] = "hello from KD9PRC hello from KD9PRC hello from KD9PRC ";
+    //uint8_t msg[] = { 0x00, 0x00, 0x00, 0x01, 0x01 };
 
 
 	si4060_setup(MOD_TYPE_OOK);
@@ -308,20 +308,32 @@ void STABBY_ook(void) {
 	STABBY_setModemOOK();
 	HAL_Delay(250); // wait for the radio to initialize...
 
-    uint16_t c = 129;
+    uint16_t c = 65;
     uint16_t all = (sizeof(msg)+7)/8;
 
 
     //chRegSetThreadName("radio_tx_feeder");
     STABBY_radioTune(438680000, 0, /* power */ 127);
     // Initial FIFO fill
-    STABBY_Si4464_writeFIFO(msg, c);
+    //STABBY_Si4464_writeFIFO(msg, c);
+    int y;
+    for (y = 0; y < 65; y++) {
+        STABBY_Si4464_writeFIFO(msg[y],1);
+    }
 	ledOnRed();
 	HAL_Delay(250);
 	ledOffRed();
     // Start transmission
 
-    STABBY_si4060_start_tx(all);
+    STABBY_si4060_start_tx(10000);
+    int xx;
+	for (xx = 0; xx < 50; xx++) {
+		ledOnRed();
+		STABBY_Si4464_writeFIFO(msg, 10);
+		HAL_Delay(20);
+		ledOffRed();
+	}
+	ledOnYellow();
 /*
     while(c < all) { // Do while bytes not written into FIFO completely
             // Determine free memory in Si4464-FIFO
@@ -338,7 +350,8 @@ void STABBY_ook(void) {
 
     // Shutdown radio (and wait for Si4464 to finish transmission)
     //shutdownRadio();
-    HAL_Delay(2000);
+    HAL_Delay(7000);
+	ledOffYellow();
     si4060_stop_tx();
 
 }
